03bbbe2c4cb5541427259752c442750bba7b6c9d
Mariano Ovelar <marianoovelar20@outlook.es>
2018-08-29 12:10:04 -0300

    clase4

---
+++ Nueva carpeta/lab2-master/ovelar.mariano.2A/Entidades/Cosa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicas.en.clases
+{
+    class Cosa
+    {
+    }
+}
---
+++ Nueva carpeta/lab2-master/ovelar.mariano.2A/practicas.en.clases/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicas.en.clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Sello.imprimir();
+             Console.ReadKey();*/
+
+            //Cosa newCosa = new Cosa();
+
+           // Console.WriteLine(Cosa.mostrar(newCosa);
+
+        }
+    }
+}
